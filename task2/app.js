4d92a93e587db7273e15f703ddbfa2235df1f6b3
Tomiris21 <zhandostomi19@gmail.com>
2024-10-26 21:29:57 +0500

    2task

---
+++ task2/app.js
@@ -0,0 +1,5 @@
+const words = ["a", 'b', 'v','f'];
+const randomIndex = Math.floor(Math.random() * words.length);
+const randomWord = words[randomIndex];
+
+console.log("Случайное слово из массива: " + randomWord);
